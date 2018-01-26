c3a97444cb575c294f2b5292fc97fcce24f99f05
Cesar Cueva Mendoza <cueva1487@gmail.com>
2018-01-25 23:53:22 -0500

    reading_user_input.py

---
+++ reading_user_input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+name  = raw_input("what is your name?: ")
+birthdate = raw_input("what is your birthdate?: ")
+
+age = input("how old are you?: ")
+print ("%s was born on %s " % (name, birthdate))
+print ("half of your is %s " % (age/2))
