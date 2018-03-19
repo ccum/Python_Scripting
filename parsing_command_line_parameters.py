ac6cc6438106f6d905b8cef9da678db6f12e2d6b
Cesar Cueva Mendoza <cueva1487@gmail.com>
2018-03-18 21:23:52 -0500

    change

--- parsing_command_line_parameters.py
+++ parsing_command_line_parameters.py
@@ -11,14 +11,20 @@ parse.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
 args=parse.parse_args()
 print(args.filename)
 
-with open(args.filename) as f:
-    lines = f.readlines()
-    lines.reverse()
-
-    if args.limit:
+try:
+    f=open(args.filename)
+except IOError as err:
+    print("Error: file no found")
+else:
+    with f:
+        limit = args.limit
+        lines = f.readlines()
+        lines.reverse()
+    if limit:
         lines = lines[:args.limit]
     
     for line in lines:
         print(line.strip()[::-1])
-
+finally:
+    print("\n We're finished\n")
 #chmod u+x parsing_ command_line_parameters.py
