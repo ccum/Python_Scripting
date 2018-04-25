c7206dcb99cb106991455dfc60757404c4eec982
Cesar Cueva <cueva1487@gmail.com>
2018-04-24 19:17:56 -0500

    cambios on tdd

--- code/pgbackup/src/pgbackup/cli.py
+++ code/pgbackup/src/pgbackup/cli.py
@@ -1,8 +1,12 @@
 import argparse
 
+known_drivers = ['local', 's3']
+
 class DriverAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         driver, destination = values
+        if driver.lower() not in known_drivers:
+            parser.error("Unknown driver. Available drivers are 'local' & 's3'")
         namespace.driver = driver.lower()
         namespace.destination = destination
 
@@ -18,3 +22,4 @@ def create_parser():
             action=DriverAction,
             required=True)
     return parser
+
